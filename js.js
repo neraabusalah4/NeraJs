bddf79068f216a753882c828d95b5415865a6ca9
neraabusalah4 <neraabusalah@gmail.com>
2024-07-03 20:07:14 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function firstClass(){   //ex1
+let firstClassInput=document.getElementById('firstClassinput.value');
+firstClassName=firstClasInput.value;
+let firstClassParagraphs=document.getElementById('text');
+firstClassParagraphs.firstClassList.add(firstClassName);
+}
+function changeTextanAndDisable(){    //ex2
+const button=document.getElementById('myButton');
+button.textContent='click!';
+button.style.pointerEvents='none';
+
+}
+
+function NeraVisibility(elementId){   //3
+let element=document.getElementById(elementId);
+element.firstClass.Nera("hidden");
+}
+
